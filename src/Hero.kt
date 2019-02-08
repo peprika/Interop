45fa034d4360bef8d83ac3b7f4139cd596c05703
Riku Pepponen <riku.pepponen@gmail.com>
2019-02-08 17:20:58 +0200

    Uh oh - this null value crashes the program

--- src/Hero.kt
+++ src/Hero.kt
@@ -1,4 +1,7 @@
 fun main(args: Array<String>) {
     val adversary = Jhava()
     println(adversary.utterGreeting())
+
+    val friendshipLevel = adversary.determineFriendshipLevel()
+    println(friendshipLevel.toLowerCase())
 }
